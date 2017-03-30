815b78ced04754700bfef029f2486f747668e23c
lyrachord <lyrachord@outlook.com>
2017-03-30 23:05:25 +0800

    fix path after mv in build.bat

--- sample/build.bat
+++ sample/build.bat
@@ -2,7 +2,7 @@
 rem -L/IMPLIB
 rem ..\..\..\windows\bin\dmd test.d mydll.lib
 echo compiling...
-dmd -m64 -shared -ofsample_extension.dll src\d\dart.d src\d\dll.d dart\lib\x64\dart.lib sample_extension.def %1 %2 %3 %4 %5
+dmd -m64 -shared -ofsample_extension.dll ..\src\d\dart.d dll.d ..\dart\lib\x64\dart.lib sample_extension.def %1 %2 %3 %4 %5
 
 if %errorlevel% neq 0 goto EXIT
 rem dart main.dart
